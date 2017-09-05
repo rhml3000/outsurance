a0793535e988a82520d86f70dc837449621dae6b
Reuel Lawrence <reuel.lawrence@entelect.co.za>
2017-09-05 22:36:43 +0200

    Removing the fn/ln differentiator

--- OutsuranceAssessment.Models/PersonName.cs
+++ OutsuranceAssessment.Models/PersonName.cs
@@ -4,7 +4,6 @@ namespace OutsuranceAssessment.Models
     public class PersonName
     {
         public string Name { get; set; }
-        public string Type { get; set; }
         public int Count { get; set; }
     }
 }
